f320de003a4d79cf0289c35d775c76ffbeb27e43
nicknickovich <nick.poberezhnyk@gmail.com>
2019-12-11 10:33:34 +0200

    "Add department" now works as intended. Added functionality to show employees and add new employees.

--- department_app/views.py
+++ department_app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, redirect, flash
 from department_app.models import Employee, Department
 from department_app import db, app
-from department_app.forms import AddDepartmentForm
+from department_app.forms import AddDepartmentForm, AddEmployeeForm
 
 
 @app.route("/")
@@ -14,6 +14,26 @@ def show_employees():
     employees = Employee.query.order_by(Employee.id).all()
     return render_template("employees.html", employees=employees,
                             title="All employees")
+
+
+@app.route("/add_employee", methods=["GET", "POST"])
+def add_employee():
+    form = AddEmployeeForm()
+    if form.validate_on_submit():
+        Employee = Employee(
+            name=form.name.data,
+            date_of_birth=form.date_of_birth.data,
+            salary=form.salary.data,
+            department_id=form.department_id.data,
+        )
+        db.session.add(employee)
+        db.session.commit()
+        return redirect(url_for("show_employees"))
+
+    return render_template(
+        "add_employee.html", title="Add new employee",
+        form=form, legend="New Employee"
+    )
 
 
 @app.route("/departments")
@@ -30,7 +50,6 @@ def add_department():
         department = Department(name=form.name.data)
         db.session.add(department)
         db.session.commit()
-        flash("Post has been created!", "success")
         return redirect(url_for("show_departments"))
 
     return render_template(
