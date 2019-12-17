820080e86e10f9e4996cd23dace7bb6e00d80c32
nicknickovich <nick.poberezhnyk@gmail.com>
2019-12-17 11:14:05 +0200

    Reorganized project structure using blueprints.

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -3,12 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
-
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("config")
 app.config.from_pyfile("config.py")
 db = SQLAlchemy(app)
 
-# Imported after creating db and app instances
-# to avoid circular import
-from department_app import views
+from department_app.main.views import main
+from department_app.employees.views import emp
+from department_app.departments.views import dep
+
+app.register_blueprint(main)
+app.register_blueprint(emp)
+app.register_blueprint(dep)
